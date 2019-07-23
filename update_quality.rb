16bb32c4e65392c311981e884ec368f57ad29dae
daniel <g.thirteen333@gmail.com>
2019-07-23 01:10:11 -0400

    notes added

--- update_quality.rb
+++ update_quality.rb
@@ -1,15 +1,16 @@
 require 'award'
 
-def update_quality(awards)
+def update_quality(awards) 
  
-  awards.each do |award|
-   
-    return award if ("Blue Distinction Plus" == award.name)
+  awards.each do |award| # O(n) - Linear - Time Complexity  ( No nested loops )
+                         # O(1) - Space Complexity - ( No additional data structures which are consuming memory )
 
-    blue_first =            ("Blue First" == award.name)
-    blue_star =             ("Blue Star" == award.name)
+    return award if ("Blue Distinction Plus" == award.name)  # Return immediately for performance 
+
+    blue_first =            ("Blue First" == award.name)  
+    blue_star =             ("Blue Star" == award.name)    
     blue_compare =          ("Blue Compare" == award.name)
-    quality_in_bounds =     (award.quality > 0 && award.quality < 50)
+    quality_in_bounds =     (award.quality > 0 && award.quality < 50) 
 
     unless (!quality_in_bounds)
       if (blue_compare)
@@ -28,30 +29,16 @@ def update_quality(awards)
     award.expires_in -= 1
     expired = (award.expires_in < 0)
    
-    unless(!expired)
-      
-      if !blue_first
-        if !blue_compare
-          award.quality -= 1 if quality_in_bounds && !blue_star
-          award.quality -= 2 if quality_in_bounds && blue_star
-          
-        else
-          
-          award.quality -= award.quality
-        end
-      else
-       award.quality += 1 if award.quality < 50
-  
+    unless (!expired)
+      if (blue_first)
+        award.quality += 1 if award.quality < 50
+      elsif (!blue_first && !blue_compare && quality_in_bounds)
+        award.quality -= 1 if !blue_star
+        award.quality -= 2 if blue_star
+      else 
+         award.quality -= award.quality
       end
     end
-   
+    
   end
-  
-  
-  
-  
-  
-  
-  
-  
 end
