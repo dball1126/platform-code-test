f0a79885fae87f1a347e3faa7d5abcbd5f0f9bd7
daniel <g.thirteen333@gmail.com>
2019-07-22 23:43:48 -0400

    all tests pass

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,57 @@
 require 'award'
 
 def update_quality(awards)
+ 
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
+   
+    return award if ("Blue Distinction Plus" == award.name)
+
+    blue_first =            ("Blue First" == award.name)
+    blue_star =             ("Blue Star" == award.name)
+    blue_compare =          ("Blue Compare" == award.name)
+    quality_in_bounds =     (award.quality > 0 && award.quality < 50)
+
+    unless (!quality_in_bounds)
+      if (blue_compare)
         award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
+        award.quality += 1 if award.expires_in < 11
+        award.quality += 1 if award.expires_in < 6
+      elsif (!blue_first && !blue_compare && blue_star)
+        award.quality -=2
+      elsif(!blue_first && !blue_compare && !blue_star)
+        award.quality -=1
+      else
+        award.quality += 1
       end
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
+    
+    award.expires_in -= 1
+    expired = (award.expires_in < 0)
+   
+    unless(!expired)
+      
+      if !blue_first
+        if !blue_compare
+          award.quality -= 1 if quality_in_bounds && !blue_star
+          award.quality -= 2 if quality_in_bounds && blue_star
+          
         else
-          award.quality = award.quality - award.quality
+          
+          award.quality -= award.quality
         end
       else
-        if award.quality < 50
-          award.quality += 1
-        end
+       award.quality += 1 if award.quality < 50
+  
       end
     end
+   
   end
+  
+  
+  
+  
+  
+  
+  
+  
 end
